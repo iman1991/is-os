4b03dcf07915f629acdffc08a962998bff3a7548
iman1991 <iman1991@mail.ru>
2015-01-30 14:23:49 +0300

    Update int_ff.asm

--- int_ff.asm
+++ int_ff.asm
@@ -1,22 +1,14 @@
-oldseg08 dw 0
-oldofs08 dw 0
-kernel: 
-mov ax,0x2000
-
-ret
-
-new_int:
+new_int30:
 push    ds
         xor     bx,bx
         mov     ds,bx
-        mov     bx,[ds:0020h]
-        mov     es,[ds:0022h]
-        mov     [cs:oldseg08],es
-        mov     [cs:oldofs08],bx
-        xor     bx,bx
-        mov     ds,bx
-        mov     bx,[cs:kernel] ;    
-        mov     [ds:0020h],bx    
-        mov     [ds:0022h],cs
+        mov     si,run  
+        mov     [ds:00C0h],si    
+        mov     [ds:00C2h],cs
         pop     ds
 ret
+
+int30 
+ pop ax
+ pop ax
+ jmp dword 0x0:run
