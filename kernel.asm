9e817d035510757b0e40cc5a36a17561d62395c1
Iman <iman1991@mail.ru>
2015-02-03 00:44:27 +0300

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -10,29 +10,41 @@ mov si,msg_sys
 call writeln
 mov cx,0
 mov [icom],cx
+;printf msg_sys
+mov ax,cs
 
-call new_int30
+
+call new_int30;
+
+mov ah,0x1
+int 30h
 
 run:
- call wr_inf
- push ax
- mov si,rt
- call write
- call read_key
- pop ax  
- call cmd
-
+;call wr_inf
+push ax
+mov si,rt
+call write
+call read_key
+pop ax  
+call cmd
 jmp run 
+seg_krn:
+ dw 0;
+ 
+
+
 
 rt:   db '~$ ',0 ;
 msg:
 db '...done!',0
 msg_sys:
-db 'the system is running',0
-;%include "data.inc"
+db 'the system is booting',0
+%include "data.inc"
 %include "command.asm"
 %include "unit.asm"
 %include "util.asm"
 %include "disk_fs.asm"
 %include "trask.asm"
 %include "int_ff.asm"
+%include  "mem.asm"
+bufer:
