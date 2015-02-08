77fde6c5190300f6a7a8bac797612f1ea8decb2a
Iman <iman1991@mail.ru>
2015-02-09 00:41:38 +0300

    Update mem.asm

--- mem.asm
+++ mem.asm
@@ -1,4 +1,16 @@
-size_str: db 0xff
-count_mem: dw 0x300
-mem_tab: dw 0xfb00
+size_str        equ 0xff ;размер блоков 
+count_mem       equ 0x303 ; количество блоков
+mem_tab         equ 0xfb00 ; адрес таблицы блоков 
+KERNEL_SEGMENT  equ 0x50 ; сегмент ядра 
+KERNEL_SIZE     equ 4096 ; размер ядра без драйверов 
 
+
+
+
+
+
+;0000h- bios
+;0x500 - IS_OS
+;1500h-faffh- driver
+;cs-1000h,2000h,3000h,4000
+;0000h-ffffh- bufer
