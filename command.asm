5b774f01e03a9d6b4dde90f4b4b946368992dfc0
iman1991 <iman1991@mail.ru>
2017-08-02 16:23:19 +0300

    new commit

--- command.asm
+++ command.asm
@@ -1,7 +1,6 @@
 section .text
 use16
 cclr:   db 'clr',0
-cexit:  db 'exit',0
 crun:   db 'run',0
 cfrmd:  db 'format',0
 cnf:    db 'nf',0
@@ -9,32 +8,32 @@ cfind:  db 'find',0
 cerror:  db 'error command',0
 
 cmd: ; обработка команд
-	xor cx,cx
- 	mov si,cx
+	xor cx, cx
+ 	mov si, cx
  	push ax
 
 
 inf: 
-	mov al,[command+si]
- 	mov ah,[cnf+si]
- 	cmp al,ah
+	mov al, [command+si]
+ 	mov ah, [cnf+si]
+ 	cmp al, ah
  	jne irun;
 	inc si
- 	cmp si,2
+ 	cmp si, 2
  	jl inf
 	call new_file
 	jmp exit_cmd  
 
 irun: 
- 	mov al,[command+si]
- 	mov ah,[crun+si]
- 	cmp al,ah
+ 	mov al, [command+si]
+ 	mov ah, [crun+si]
+ 	cmp al, ah
  	jne iclr;
 	inc si
- 	cmp si,3
+ 	cmp si, 3
  	jl irun
 	pop ax 
-	mov ax,0xffff
+	mov ax, 0xffff
 	push ax 
 	jmp exit_cmd  
 
@@ -42,7 +41,7 @@ iclr:
  	mov al,[command+si]
  	mov ah,[cclr+si]
 	 cmp al,ah
- 	jne iexit;
+ 	jne ifind;
  	inc si
  	cmp si,3
  	jl iclr
@@ -53,20 +52,6 @@ iclr:
  	xor dx,dx
  	mov [mx],dx 
  	jmp exit_cmd
-
-iexit:
- 	mov al,[command+si]
- 	mov ah,[cexit+si]
- 	cmp al,ah
- 	jne ifind;
- 	inc si
- 	cmp si,4
- 	jl iexit
- 	mov ax,0x5307
- 	mov bx,0x0003
- 	mov cx,0x0001
- 	int 0x15 ; переривание биос выключение ПК
-	 jmp exit_cmd
 
 ifind: 
  	mov al,[command+si]
@@ -84,7 +69,7 @@ ifrmd:
  	mov al, [command+si]
  	mov ah, [cfrmd+si]
  	cmp al, ah
- 	jne ihelp;
+ 	jne ihelp
  	inc si
  	cmp si, 6
  	jl ifrmd
